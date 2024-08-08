cde1f26220972705563772de552871f27b037d95
Hansana Sandakelum <95872422+HansanaSandakelum@users.noreply.github.com>
2024-08-08 11:06:21 +0530

    Add MainHeader component for layout

---
+++ components/main-header.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import React from "react";
+import logoImg from "@/assets/logo.png";
+
+const MainHeader = () => {
+  return (
+    <header>
+      <Link href="/">
+        <img src={logoImg.src} alt="NextLevel Food" />
+        NextLevel Food
+      </Link>
+
+      <nav>
+        <ul>
+          <li>
+            <Link href="/meals">Browse Meals</Link>
+          </li>
+          <li>
+            <Link href="/community">Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
